c9964b74b316dcd1ea67fb7526642a483470c577
Angelo <tozza.angelo@gmail.com>
2019-12-12 15:19:21 +0100

    Corrected db

--- db/smartorganizerdb.sql
+++ db/smartorganizerdb.sql
@@ -1,13 +1,15 @@
--- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `smartorganizerdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `smartorganizerdb`;
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
 --
 -- Host: localhost    Database: smartorganizerdb
 -- ------------------------------------------------------
--- Server version	8.0.15
+-- Server version	8.0.16
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,17 +23,17 @@
 
 DROP TABLE IF EXISTS `employees`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `employees` (
   `idemployee` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
-  `surname` varchar(45) DEFAULT NULL,
-  `fiscalcode` varchar(45) DEFAULT NULL,
-  `worksector` varchar(45) DEFAULT NULL,
-  `position` varchar(45) DEFAULT NULL,
-  `numberofregistration` varchar(45) DEFAULT NULL,
+  `name` varchar(45) NOT NULL,
+  `surname` varchar(45) NOT NULL,
+  `fiscalcode` varchar(45) NOT NULL,
+  `worksector` varchar(45) NOT NULL,
+  `position` varchar(45) NOT NULL,
+  `numberofregistration` varchar(45) NOT NULL,
   PRIMARY KEY (`idemployee`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +42,7 @@ CREATE TABLE `employees` (
 
 LOCK TABLES `employees` WRITE;
 /*!40000 ALTER TABLE `employees` DISABLE KEYS */;
-INSERT INTO `employees` VALUES (1,'de','de','de','de','de','de'),(2,'se','se','se','se','se','se'),(3,'we','we','we','we','we','we'),(4,'qw','qw','qw','qw','qw','qw');
+INSERT INTO `employees` VALUES (1,'root','root','','','',''),(3,'prova2','prova2','prova2','prova2','','');
 /*!40000 ALTER TABLE `employees` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -50,15 +52,15 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `location`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `location` (
-  `idlocation` int(11) NOT NULL,
+  `idlocation` int(11) NOT NULL AUTO_INCREMENT,
   `address` varchar(45) NOT NULL,
   `city` varchar(45) NOT NULL,
   `typeoflocation` varchar(45) NOT NULL,
   `worktype` varchar(45) NOT NULL,
   PRIMARY KEY (`idlocation`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,6 +69,7 @@ CREATE TABLE `location` (
 
 LOCK TABLES `location` WRITE;
 /*!40000 ALTER TABLE `location` DISABLE KEYS */;
+INSERT INTO `location` VALUES (1,'prova','prova','prova','prova'),(3,'prova3','prova','prova3','prova3'),(4,'via prova','prova','prova','prova');
 /*!40000 ALTER TABLE `location` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -76,13 +79,13 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `tools`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tools` (
   `idtools` int(11) NOT NULL AUTO_INCREMENT,
   `toolsname` varchar(45) DEFAULT NULL,
   `rawmaterial` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idtools`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,7 +94,7 @@ CREATE TABLE `tools` (
 
 LOCK TABLES `tools` WRITE;
 /*!40000 ALTER TABLE `tools` DISABLE KEYS */;
-INSERT INTO `tools` VALUES (1,'martello',NULL),(2,'cacciavite',NULL),(3,'sega',NULL),(4,'vite','');
+INSERT INTO `tools` VALUES (1,'martello','martello'),(2,'cacciavite',NULL),(3,'prova1','prova1'),(4,'prova','prova'),(5,'prova','prova'),(7,'provauser','provauser'),(8,'prova3','prova2'),(10,'prova0','prova0'),(11,'','');
 /*!40000 ALTER TABLE `tools` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -101,14 +104,14 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
   `usertype` varchar(45) DEFAULT NULL,
   `password` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -117,7 +120,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'admin','ADMIN','admin'),(2,'user','USER','user'),(3,'prova','USER','prova');
+INSERT INTO `user` VALUES (1,'admin','ADMIN','admin'),(2,'user','USER','user'),(4,'prova2','USER','prova2');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -130,4 +133,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-12-12  9:57:00
+-- Dump completed on 2019-12-12 15:18:20
