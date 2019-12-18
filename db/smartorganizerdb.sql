53312c5c247e950899815b52a2f05510eb21dfdd
Ivan <emac700@gmail.com>
2019-12-18 15:21:01 +0100

    database increment 2

--- db/smartorganizerdb.sql
+++ db/smartorganizerdb.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `smartorganizerdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `smartorganizerdb`;
 -- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
 --
 -- Host: localhost    Database: smartorganizerdb
@@ -210,4 +212,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-12-18 15:13:09
+-- Dump completed on 2019-12-18 15:20:18
