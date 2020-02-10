c87912a9c81ac66187a0f28c0717672686d47a06
emac700 <emac700@gmail.com>
2020-02-10 17:29:14 +0100

    login and routing

--- Angular0/smartOrganizer/src/app/app.module.ts
+++ Angular0/smartOrganizer/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    
     NgbModule,
+    LoginModule,
     BrowserModule,
     AppRoutingModule
   ],
